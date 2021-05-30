b561a3670868ee7eab4d1bbeb99093ef8b57b269
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-05-30 12:37:34 +0300

    task-1 final

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,18 @@
 
-const liItemRef = document.querySelectorAll('.item');
-console.log(`В списке ${liItemRef.length} категории.`);
+const ref= document.querySelectorAll('li.item');
+
+function getCategoriesCount() {
+    console.log(`В списке ${ref.length} категории.`);
+}
+
+function getCategegories() {
+    ref.forEach(element => {
+        console.log(`Категория: ${element.firstElementChild.textContent}`);
+        console.log(`Количество элементов: ${element.lastElementChild.children.length}`);
+    });
+    
+}
+
+getCategoriesCount();
+
+getCategegories();
